9020c040915cdd8326e7d1d79ce74270b4d4d1ed
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-21 14:30:11 -0700

    added a profiles page
    will add as many birthdays as there are in data

---
+++ 2022_12_20/profiles.js
@@ -0,0 +1,36 @@
+const data = [
+  { name: "Bertie Yates", age: 29 },
+  { name: "Hester Hogan", age: 32 },
+  { name: "Larry Little", age: 36 },
+  { name: "Sean Walsh", age: 34 },
+  { name: "Lola Gardner", age: 29 },
+  { name: "Ipsum Loren", age: 24 },
+  { name: "Ipsum Loren", age: 41 },
+  { name: "Ipsum Loren", age: 28 },
+  { name: "Ipsum Loren", age: 37 },
+];
+
+const total = document.querySelector("h1");
+const profilesContainer = document.querySelector(".profiles");
+let profiles = document.querySelectorAll(".profile");
+const profile = document.querySelector(".clone");
+let profileClone = profile.cloneNode(true);
+
+while (profiles.length < data.length) {
+  profileClone = profile.cloneNode(true);
+  profilesContainer.appendChild(profileClone);
+  profiles = document.querySelectorAll(".profile");
+}
+
+if (profiles.length == data.length) {
+  const names = document.querySelectorAll(".name");
+  const ages = document.querySelectorAll(".info p:last-child");
+
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    names[index].innerHTML = element.name;
+    ages[index].innerHTML = element.age + " years old";
+  }
+}
+
+total.innerHTML = `${data.length} Birthdays Today`;
---
+++ 2022_12_20/script.js
@@ -0,0 +1,24 @@
+const data = [
+  { name: "Bertie Yates", age: 29 },
+  { name: "Hester Hogan", age: 32 },
+  { name: "Larry Little", age: 36 },
+  { name: "Sean Walsh", age: 34 },
+  { name: "Lola Gardner", age: 29 },
+  { name: "Ipsum Loren", age: 24 },
+  { name: "Ipsum Loren", age: 41 },
+  { name: "Ipsum Loren", age: 28 },
+  { name: "Ipsum Loren", age: 37 },
+];
+
+const names = document.querySelectorAll(".name");
+const ages = document.querySelectorAll(".info p:last-child");
+const total = document.querySelector("h1");
+const profiles = document.querySelectorAll(".profile");
+
+for (let index = 0; index < profiles.length; index++) {
+  const element = data[index];
+  names[index].innerHTML = element.name;
+  ages[index].innerHTML = element.age + " years old";
+}
+
+total.innerHTML = `${data.length} Birthdays Today`;
