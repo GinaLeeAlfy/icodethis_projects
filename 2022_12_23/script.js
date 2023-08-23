6541534f94c3812976caab365bffd8e8cdf52b92
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-23 16:28:20 -0700

    starting to add functionality

---
+++ 2022_12_23/script.js
@@ -0,0 +1,51 @@
+const data = [
+  {
+    name: "Ronald A. Freeman",
+    job: "Electronic technician",
+    img: "https://static-00.iconduck.com/assets.00/person-tipping-hand-dark-emoji-256x256-yvud7kni.png",
+    imgAlt: "emoji of man with hand up",
+    review:
+      "Thank you! Your product descriptions are amazing and your service is wonderful.",
+  },
+  {
+    name: "Alice L. Tinker",
+    job: "Marine equipment mechanic",
+    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
+    imgAlt: "emoji of blonde woman raising hand.",
+    review:
+      "Would definitely recommend ItemScribe and will definitely be ordering again.",
+  },
+  {
+    name: "Name A. Last",
+    job: "Random Job",
+    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
+    imgAlt: "emoji of blonde woman raising hand.",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, consectetur!",
+  },
+  {
+    name: "First B. Done",
+    job: "Job Random",
+    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
+    imgAlt: "emoji of blonde woman raising hand.",
+    review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+  },
+  {
+    name: "Name C. End",
+    job: "Random* Job",
+    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
+    imgAlt: "emoji of blonde woman raising hand.",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nulla assumenda velit nostrum!",
+  },
+  {
+    name: "First D. Last",
+    job: "Random 6 Job",
+    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
+    imgAlt: "emoji of blonde woman raising hand.",
+    review: "Lorem ipsum dolor sit amet!!!!",
+  },
+];
+
+const left = document.querySelector(".scroll button:first-child");
+const right = document.querySelector(".scroll button:last-child");
