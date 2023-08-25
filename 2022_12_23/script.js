057cc0d65c8c3b26f249e1976a65e13d7612cdde
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-25 11:53:30 -0700

    used pagination instead of trying to scroll through contents with order

--- 2022_12_23/script.js
+++ 2022_12_23/script.js
@@ -18,31 +18,31 @@ const data = [
   {
     name: "Name A. Last",
     job: "Random Job",
-    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
-    imgAlt: "emoji of blonde woman raising hand.",
+    img: "https://images.emojiterra.com/twitter/v14.0/512px/1f3c4-2642.png",
+    imgAlt: "emoji of surfer",
     review:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe, consectetur!",
   },
   {
     name: "First B. Done",
     job: "Job Random",
-    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
-    imgAlt: "emoji of blonde woman raising hand.",
+    img: "https://images.unsplash.com/photo-1524117074681-31bd4de22ad3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2104&q=80",
+    imgAlt: "woman with emoji face balloon",
     review: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
   },
   {
     name: "Name C. End",
     job: "Random* Job",
-    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
-    imgAlt: "emoji of blonde woman raising hand.",
+    img: "https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/family-man-girl-girl.png",
+    imgAlt: "emoji of man with two daughters",
     review:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nulla assumenda velit nostrum!",
   },
   {
     name: "First D. Last",
     job: "Random 6 Job",
-    img: "https://www.emoji.co.uk/files/apple-emojis/smileys-people-ios/147-happy-person-raising-one-hand.png",
-    imgAlt: "emoji of blonde woman raising hand.",
+    img: "https://img.icons8.com/emoji/12x/woman-running.png",
+    imgAlt: "emoji of woman running",
     review: "Lorem ipsum dolor sit amet!!!!",
   },
 ];
@@ -65,6 +65,50 @@ let orders = [];
 const PAGINATION_LIMIT = 2;
 let pageCount;
 let currentPage = 1;
+let turns = 2;
+
+function populatePageContent() {
+  while (contents.length < data.length) {
+    contentClone = contentToCopy.cloneNode(true);
+    contentClone.classList.add("hidden");
+    contentContainer.appendChild(contentClone);
+    contents = document.querySelectorAll(".content");
+  }
+
+  if (contents.length == data.length) {
+    pageCount = Math.ceil(contents.length / PAGINATION_LIMIT);
+
+    const names = document.querySelectorAll(".info p:first-child");
+    const jobs = document.querySelectorAll(".info p:last-child");
+    const imgs = document.querySelectorAll("img");
+    const reviews = document.querySelectorAll(".review");
+    //populate contents
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      names[index].innerHTML = element.name;
+      jobs[index].innerHTML = element.job;
+      reviews[index].innerHTML = element.review;
+      imgs[index].src = element.img;
+      imgs[index].alt = element.imgAlt;
+    }
+  }
+
+  while (pagesButtons.length < pageCount) {
+    pageButtonClone = pageButtonToCopy.cloneNode(true);
+    pageButtonClone.childNodes[1].classList.remove("active");
+    pagesContainer.appendChild(pageButtonClone);
+    pagesButtons = document.querySelectorAll(".pages button");
+  }
+
+  if (pagesButtons.length == pageCount) {
+    for (let index = 0; index < pagesButtons.length; index++) {
+      const element = pagesButtons[index];
+      element.classList.add("pagination-number");
+      element.setAttribute("page-index", index + 1);
+      element.setAttribute("aria-label", "Page " + (index + 1));
+    }
+  }
+}
 
 const removeActive = () => {
   circles = document.querySelectorAll(".circle");
@@ -73,101 +117,77 @@ const removeActive = () => {
   });
 };
 
-while (contents.length < data.length) {
-  contentClone = contentToCopy.cloneNode(true);
-  contentClone.classList.add("hidden");
-  contentContainer.appendChild(contentClone);
-  contents = document.querySelectorAll(".content");
-}
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
 
-for (let index = 0; index < data.length; index++) {
-  orders.push(index);
-  contents[index].style.order = orders[index];
-}
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
 
-if (contents.length == data.length) {
-  pageCount = Math.ceil(contents.length / PAGINATION_LIMIT);
-
-  const names = document.querySelectorAll(".info p:first-child");
-  const jobs = document.querySelectorAll(".info p:last-child");
-  const imgs = document.querySelectorAll("img");
-  const reviews = document.querySelectorAll(".review");
-  //populate contents
-  for (let index = 0; index < data.length; index++) {
-    const element = data[index];
-    names[index].innerHTML = element.name;
-    jobs[index].innerHTML = element.job;
-    reviews[index].innerHTML = element.review;
-    imgs[index].src = element.img;
-    imgs[index].alt = element.imgAlt;
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(left);
+  } else {
+    enableButton(left);
   }
-}
 
-while (pagesButtons.length < pageCount) {
-  pageButtonClone = pageButtonToCopy.cloneNode(true);
-  pageButtonClone.childNodes[1].classList.remove("active");
-  pagesContainer.appendChild(pageButtonClone);
-  pagesButtons = document.querySelectorAll(".pages button");
-}
+  if (pageCount === currentPage) {
+    disableButton(right);
+  } else {
+    enableButton(right);
+  }
+};
 
-right.addEventListener("click", (event) => {
-  for (let index = 0; index < contents.length; index++) {
-    const element = contents[index];
-    if (element.style.order == orders[orders.length - 1]) {
-      element.style.order = 0;
-    } else {
-      element.style.order++;
-    }
-    if (element.style.order <= 1) {
-      element.classList.remove("hidden");
-    } else {
-      element.classList.add("hidden");
+const handleActivePageNumber = () => {
+  removeActive();
+  for (let page = 0; page < pagesButtons.length; page++) {
+    const element = pagesButtons[page];
+    const pageIndex = Number(element.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      circles[page].classList.add("active");
     }
   }
-});
-
-left.addEventListener("click", (event) => {
-  for (let index = 0; index < contents.length; index++) {
-    const element = contents[index];
-    if (element.style.order == 0) {
-      element.style.order = orders.length - 1;
-    } else {
-      element.style.order--;
-    }
-    if (element.style.order <= 1) {
-      element.classList.remove("hidden");
-    } else {
-      element.classList.add("hidden");
-    }
-  }
-});
-
-for (let index = 0; index < pagesButtons.length; index++) {
-  const element = pagesButtons[index];
-  element.classList.add("pagination-number");
-  element.setAttribute("page-index", index + 1);
-  element.setAttribute("aria-label", "Page " + (index + 1));
-}
+};
 
 const setCurrentPage = (pageNum) => {
   currentPage = pageNum;
 
-  const prevRange = (pageNum - 1) * PAGINATION_LIMIT;
-};
-for (let page = 0; page < pagesButtons.length; page++) {
-  const element = pagesButtons[page];
-  element.addEventListener("click", (event) => {
-    removeActive();
-    circles[page].classList.add("active");
+  handleActivePageNumber();
+  handlePageButtonsStatus();
 
-    for (let index = 0; index < contents.length; index++) {
-      const element = contents[index];
-      element.style.order = index;
-      if (element.style.order <= 1) {
-        element.classList.remove("hidden");
-      } else {
-        element.classList.add("hidden");
-      }
+  const prevRange = (pageNum - 1) * PAGINATION_LIMIT;
+  const currRange = pageNum * PAGINATION_LIMIT;
+
+  contents.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
     }
   });
-}
+};
+
+window.addEventListener("load", () => {
+  populatePageContent();
+  setCurrentPage(1);
+
+  left.addEventListener("click", (event) => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  right.addEventListener("click", (event) => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  pagesButtons.forEach((element) => {
+    const pageIndex = Number(element.getAttribute("page-index"));
+
+    if (pageIndex) {
+      element.addEventListener("click", (event) => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
