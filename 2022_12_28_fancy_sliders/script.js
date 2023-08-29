c42bd26780ae561c56c52dc8ed37aafa4dfe46bd
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-29 15:21:01 -0700

    trying to make a slider by hand

---
+++ 2022_12_28_fancy_sliders/script.js
@@ -0,0 +1,20 @@
+const slider = document.querySelector(".slider");
+const progress = document.querySelector(".progress");
+const thumb = document.querySelector(".thumb");
+let isDragging = false;
+
+progress.style.width = `${(slider.value / slider.max) * 100}%`;
+
+function adjustProgress() {
+  progress.style.width = `${(slider.value / slider.max) * 100}%`;
+}
+
+thumb.addEventListener("mousedown", beginDragging());
+
+function beginDragging(event) {
+  event.preventDefault();
+  isDragging = true;
+  document.addEventListener("mousedown", dragHandler(event));
+}
+
+function dragHandler(event) {}
