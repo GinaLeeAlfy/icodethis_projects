8450be5668f0605f7d6ba17c1ab0f7410f0208ae
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-29 16:10:15 -0700

    named some script variables

--- 2022_12_28_fancy_sliders/script.js
+++ 2022_12_28_fancy_sliders/script.js
@@ -6,7 +6,11 @@ progress.style.width = `${(slider.value / slider.max) * 100}%`;
 valueDisplay.innerHTML = `$${slider.value}`;
 
 function adjustProgress() {
-  progress.style.width = `${(slider.value / slider.max) * 100}%`;
+  let sliderValue = slider.value;
+  let sliderMax = slider.max;
+  let percentage = (sliderValue / sliderMax) * 100;
+
+  progress.style.width = `${percentage}%`;
   valueDisplay.innerHTML = `$${slider.value}`;
-  valueDisplay.style.left = `${(slider.value / slider.max) * 100 - 7}%`;
+  valueDisplay.style.left = `${percentage - 7}%`;
 }
