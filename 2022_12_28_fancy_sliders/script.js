1d6bfa220e5000223461e8b37c10708771823011
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-29 15:25:05 -0700

    initial try with input range and making my own progress bar
    moz-range-progress/track doesn't seem to be working

--- 2022_12_28_fancy_sliders/script.js
+++ 2022_12_28_fancy_sliders/script.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector(".slider");
 const progress = document.querySelector(".progress");
-const thumb = document.querySelector(".thumb");
-let isDragging = false;
+// const thumb = document.querySelector(".thumb");
+// let isDragging = false;
 
 progress.style.width = `${(slider.value / slider.max) * 100}%`;
 
@@ -9,12 +9,12 @@ function adjustProgress() {
   progress.style.width = `${(slider.value / slider.max) * 100}%`;
 }
 
-thumb.addEventListener("mousedown", beginDragging());
+// thumb.addEventListener("mousedown", beginDragging());
 
-function beginDragging(event) {
-  event.preventDefault();
-  isDragging = true;
-  document.addEventListener("mousedown", dragHandler(event));
-}
+// function beginDragging(event) {
+//   event.preventDefault();
+//   isDragging = true;
+//   document.addEventListener("mousedown", dragHandler(event));
+// }
 
-function dragHandler(event) {}
+// function dragHandler(event) {}
