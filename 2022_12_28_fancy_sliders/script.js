32cd837ca744e2b466c91392da24735f65471a80
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-08-29 15:55:16 -0700

    first slider done

--- 2022_12_28_fancy_sliders/script.js
+++ 2022_12_28_fancy_sliders/script.js
@@ -1,10 +1,12 @@
 const slider = document.querySelector(".slider");
 const progress = document.querySelector(".progress");
-// const thumb = document.querySelector(".thumb");
-// let isDragging = false;
+const valueDisplay = document.querySelector(".value-display");
 
 progress.style.width = `${(slider.value / slider.max) * 100}%`;
+valueDisplay.innerHTML = `$${slider.value}`;
 
 function adjustProgress() {
   progress.style.width = `${(slider.value / slider.max) * 100}%`;
+  valueDisplay.innerHTML = `$${slider.value}`;
+  valueDisplay.style.left = `${(slider.value / slider.max) * 100 - 7}%`;
 }
