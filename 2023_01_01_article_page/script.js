42b5b2001a6e71818763d26dff0b42487c5bc548
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-09-05 14:18:08 -0700

    edited search bar
    need to prep for mobile

--- 2023_01_01_article_page/script.js
+++ 2023_01_01_article_page/script.js
@@ -2,6 +2,8 @@ const productButton = document.querySelector("#product");
 const casesButton = document.querySelector("#cases");
 const productLinks = document.querySelector(".links");
 const casesLinks = document.querySelectorAll(".links");
+const searchBar = document.querySelector("input");
+const label = document.querySelector("label");
 
 function toggleMenu(element) {
   if (element.style.display === "block") {
@@ -11,10 +13,23 @@ function toggleMenu(element) {
   }
 }
 
+function toggleSearch(element) {
+  if (element.style.visibility === "visible") {
+    element.style.visibility = "hidden";
+  } else {
+    element.style.visibility = "visible";
+    focus(searchBar);
+  }
+}
+
 productButton.addEventListener("click", (event) => {
   toggleMenu(productLinks);
 });
 
 casesButton.addEventListener("click", (event) => {
   toggleMenu(casesLinks[1]);
+});
+
+label.addEventListener("click", (event) => {
+  toggleSearch(searchBar);
 });
