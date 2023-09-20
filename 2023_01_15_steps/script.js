d5e35354bf372b09cda270329974a7906c7869c4
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-09-20 12:14:14 -0700

    customizing different bars

--- 2023_01_15_steps/script.js
+++ 2023_01_15_steps/script.js
@@ -2,56 +2,74 @@ const review = document.querySelector(".review");
 const approve = document.querySelector(".approved");
 const secondSteps = document.querySelectorAll(".second");
 const thirdSteps = document.querySelectorAll(".third");
-const percentageDisplay = document.querySelector(".percentage-display");
-let isFinished = true;
+const secondTexts = document.querySelectorAll(".second-text");
+const thirdTexts = document.querySelectorAll(".third-text");
+const percentageDisplays = document.querySelectorAll(".percentage-display");
 
 function startProgress() {
   let width = 0;
   let time = setInterval(frame, 100);
 
   review.addEventListener("click", () => {
-    width = 50;
+    width = 100;
     secondSteps.forEach((element) => {
       element.classList.add("active");
     });
-    reviewProgress();
-  });
-
-  function frame() {
-    if (width >= 50) {
-      clearInterval(time);
-      isFinished = true;
-    } else {
-      isFinished = false;
-      width++;
-      percentageDisplay.style.width = width + "%";
-    }
-  }
-}
-
-function reviewProgress() {
-  let width = 50;
-  let time = setInterval(frame, 100);
-
-  approve.addEventListener("click", () => {
-    width = 99;
+    secondSteps[2].innerHTML = "&check;";
     thirdSteps.forEach((element) => {
-      element.classList.add("active");
+      element.classList.remove("active");
     });
+    thirdSteps[2].innerHTML = "";
+    reviewProgress();
   });
 
   function frame() {
     if (width >= 100) {
       clearInterval(time);
-      isFinished = true;
     } else {
-      isFinished = false;
       width++;
-      percentageDisplay.style.width = width + "%";
+      percentageDisplays.forEach((element) => {
+        element.style.width = width / 2 + "%";
+      });
+    }
+  }
+}
+
+function reviewProgress() {
+  secondTexts.forEach((element) => {
+    element.innerHTML = "In review";
+  });
+  thirdTexts.forEach((element) => {
+    element.innerHTML = "Needs approval";
+  });
+  approve.removeAttribute("disabled");
+  let width = 100;
+  let time = setInterval(frame, 100);
+
+  approve.addEventListener("click", () => {
+    width = 199;
+    thirdSteps.forEach((element) => {
+      element.classList.add("active");
+    });
+    thirdTexts.forEach((element) => {
+      element.innerHTML = "Approved";
+    });
+    thirdSteps[2].innerHTML = "&check;";
+  });
+
+  function frame() {
+    if (width >= 200) {
+      clearInterval(time);
+    } else {
+      width++;
+      percentageDisplays.forEach((element) => {
+        element.style.width = width / 2 + "%";
+      });
     }
   }
 }
 
 window.addEventListener("load", () => {
   startProgress();
+  approve.setAttribute("disabled", true);
 });
