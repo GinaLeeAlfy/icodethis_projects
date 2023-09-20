1d99e0cc6e5d960e43adf031647460b6ec344e35
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-09-20 11:15:04 -0700

    made buttons work

--- 2023_01_15_steps/script.js
+++ 2023_01_15_steps/script.js
@@ -1,5 +1,7 @@
 const review = document.querySelector(".review");
 const approve = document.querySelector(".approved");
+const secondSteps = document.querySelectorAll(".second");
+const thirdSteps = document.querySelectorAll(".third");
 const percentageDisplay = document.querySelector(".percentage-display");
 let isFinished = true;
 
@@ -7,25 +9,48 @@ function startProgress() {
   let width = 0;
   let time = setInterval(frame, 100);
 
+  review.addEventListener("click", () => {
+    width = 50;
+    secondSteps.forEach((element) => {
+      element.classList.add("active");
+    });
+    reviewProgress();
+  });
+
   function frame() {
-    if (width >= 100) {
+    if (width >= 50) {
       clearInterval(time);
       isFinished = true;
-      start.removeAttribute("disabled");
     } else {
       isFinished = false;
-      start.setAttribute("disabled", true);
       width++;
       percentageDisplay.style.width = width + "%";
     }
   }
 }
 
-review.addEventListener("click", () => {
-  if (isFinished) {
-    startProgress();
+function reviewProgress() {
+  let width = 50;
+  let time = setInterval(frame, 100);
+
+  approve.addEventListener("click", () => {
+    width = 99;
+    thirdSteps.forEach((element) => {
+      element.classList.add("active");
+    });
+  });
+
+  function frame() {
+    if (width >= 100) {
+      clearInterval(time);
+      isFinished = true;
+    } else {
+      isFinished = false;
+      width++;
+      percentageDisplay.style.width = width + "%";
+    }
   }
-});
+}
 
 window.addEventListener("load", () => {
   startProgress();
