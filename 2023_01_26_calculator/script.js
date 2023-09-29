e75f967a325df6d8770523f1f5fdb05bd0d7bdad
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-09-29 11:14:33 -0700

    added a handleNumber function

--- 2023_01_26_calculator/script.js
+++ 2023_01_26_calculator/script.js
@@ -1,11 +1,22 @@
 const buttons = document.querySelectorAll(".button");
+const currentScreen = document.querySelector(".current-screen");
+const totalScreen = document.querySelector(".total-screen");
+let number = "";
 
 buttons.forEach((element) => {
   element.addEventListener("click", () => {
     if (isNaN(element.innerHTML)) {
       console.log("not a number");
     } else {
-      Number.element.innerHTML;
+      newNumber = element.innerHTML;
+      handleNumber(newNumber);
     }
   });
 });
+
+function handleNumber(newNumber) {
+  number = number + newNumber;
+  console.log(number);
+}
+
+function handleOperator(operator) {}
