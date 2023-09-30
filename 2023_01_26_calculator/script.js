f4faeab7a2f4f8618ff829ff4d3c4d2f553798ff
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-09-29 17:22:22 -0700

    added most functions
    still need to fix backspace and maybe do paratheses

--- 2023_01_26_calculator/script.js
+++ 2023_01_26_calculator/script.js
@@ -1,57 +1,153 @@
 const buttons = document.querySelectorAll(".button");
 const currentScreen = document.querySelector(".current-screen");
 const totalScreen = document.querySelector(".total-screen");
-let number = "";
+let currentNumber = "";
+let displayNumber = "";
+let numbers = [];
+let operators = [];
+let pastInfo = [];
+let pastInfoTotal = [];
+let pastInfoDisplay = "";
+let total = "";
 
 buttons.forEach((element) => {
   element.addEventListener("click", () => {
     if (isNaN(element.innerHTML)) {
-      console.log(element.innerHTML);
       handleOperator(element.innerHTML);
     } else {
       handleNumber(element.innerHTML);
     }
+    displayCurrentScreen(currentNumber);
   });
 });
 
 function handleNumber(newNumber) {
-  number = number + newNumber;
-  currentScreen.innerHTML = number;
+  currentNumber = currentNumber + newNumber;
+  if (numbers.length > 0) {
+    calcTotal();
+    displayTotalScreen();
+  }
+  return currentNumber;
 }
 
 function handleOperator(operator) {
   switch (operator) {
     case "CE":
-      console.log("clear entry");
-      break;
+      currentNumber = "";
+      calcTotal();
+      displayTotalScreen();
+      return currentNumber;
     case "C":
-      console.log("reset");
+      currentNumber = "";
+      pastInfoDisplay = "";
+      numbers = [];
+      operators = [];
+      total = "";
+      displayTotalScreen();
       break;
     case "⇦":
-      console.log("back");
-      break;
+      if (currentNumber != "") {
+        currentNumber = currentNumber.substring(0, currentNumber.length - 1);
+      } else if (pastInfoDisplay != "" && currentNumber == "") {
+        operators.pop();
+        pastInfoDisplay = pastInfoDisplay.substring(
+          0,
+          pastInfoDisplay.length - 1
+        );
+        if (numbers.length != operators.length) {
+          currentNumber = numbers[numbers.length - 1];
+          numbers.pop();
+          pastInfoDisplay = pastInfoDisplay.substring(
+            0,
+            pastInfoDisplay.length - 1
+          );
+        }
+      }
+
+      if (numbers.length < 1) {
+        totalScreen.innerHTML = "";
+      } else {
+        calcTotal();
+        displayTotalScreen();
+      }
+      return currentNumber;
     case "÷":
-      console.log("/");
+      handleCalculation("/", "÷");
       break;
     case "×":
-      console.log("*");
+      handleCalculation("*", "×");
       break;
     case "−":
-      console.log("-");
+      handleCalculation("-", "−");
       break;
     case "+":
-      console.log("+");
+      handleCalculation("+", "+");
       break;
-    case "±":
-      console.log("+/-");
+    case "()":
+      if (total != "") {
+        total = total * -1;
+        displayTotalScreen();
+      }
       break;
     case ".":
-      console.log(".");
+      if (currentNumber == "") {
+        currentNumber = "0.";
+      } else {
+        handleNumber(".");
+      }
       break;
     case "=":
-      console.log("=");
+      currentNumber = total;
+      pastInfoDisplay = "";
+      numbers = [];
+      operators = [];
+      total = "";
+      displayTotalScreen();
       break;
     default:
       console.log("what is this");
   }
+}
+
+function handleCalculation(operator, operatorDisplay) {
+  //check if operator was the last thing clicked
+  if (pastInfoDisplay != "" && currentNumber == "") {
+    operators.pop();
+    operators.push(operator);
+    pastInfoDisplay = pastInfoDisplay.substring(0, pastInfoDisplay.length - 1);
+    pastInfoDisplay = pastInfoDisplay + operatorDisplay;
+  } else {
+    numbers.push(currentNumber);
+    operators.push(operator);
+    pastInfoDisplay = pastInfoDisplay + currentNumber + operatorDisplay;
+    currentNumber = "";
+    return currentNumber;
+  }
+}
+
+function displayCurrentScreen(currentNumber) {
+  currentScreen.innerHTML = pastInfoDisplay + currentNumber;
+}
+
+function displayTotalScreen() {
+  totalScreen.innerHTML = total;
+}
+
+function calcTotal() {
+  for (let index = 0; index < numbers.length; index++) {
+    pastInfo.push(numbers[index], operators[index]);
+  }
+
+  if (currentNumber == "") {
+    pastInfo.pop();
+    total = eval(pastInfo.join(""));
+  } else {
+    total = eval(pastInfo.join("") + currentNumber);
+  }
+
+  if (pastInfo.slice(-1) == "/" && currentNumber == 0) {
+    total = "Can't Divide by 0";
+  }
+  pastInfo = [];
+  return total;
 }
