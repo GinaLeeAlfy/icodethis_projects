d8c76eb1b27dbe7d8d97aa362714c34807ceee2a
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-10-11 10:01:53 -0700

    added timer when play is clicked

--- 2023_02_05_music_player/script.js
+++ 2023_02_05_music_player/script.js
@@ -8,18 +8,68 @@ const data = [
   },
 ];
 
-const songContainer = document.querySelector("ul");
-let songsList = document.querySelectorAll("li");
-const songTemplate = document.querySelector(".clone");
-let songClone = songTemplate.cloneNode(true);
+// const songContainer = document.querySelector("ul");
+// let songsList = document.querySelectorAll("li");
+// const songTemplate = document.querySelector(".clone");
+// let songClone = songTemplate.cloneNode(true);
 
-while (songsList.length < data.length) {
-  songClone = songTemplate.cloneNode(true);
-  songContainer.appendChild(songClone);
-  songsList = document.querySelectorAll("li");
+// while (songsList.length < data.length) {
+//   songClone = songTemplate.cloneNode(true);
+//   songContainer.appendChild(songClone);
+//   songsList = document.querySelectorAll("li");
+// }
+
+// if (songsList.length == data.length) {
+//   const names = document.querySelectorAll(".name");
+//   const times = document.querySelectorAll(".time");
+// }
+
+const progressBar = document.querySelector(".percentage-display");
+const playButton = document.querySelector(".play");
+const currentTimeDisplay = document.querySelector(".current-time");
+let songLength = data[0].time;
+
+let isFinished = true;
+
+function startProgress() {
+  let songTime = 0;
+  let time = setInterval(frame, 100);
+
+  function frame() {
+    if (songTime >= songLength) {
+      clearInterval(time);
+      isFinished = true;
+    } else {
+      isFinished = false;
+      songTime++;
+
+      let songTimeMinutes;
+
+      if (songTime % 60 === 0) {
+        songTimeMinutes = Math.round(songTime / 60);
+      } else {
+        songTimeMinutes = Math.floor(songTime / 60);
+      }
+
+      let songTimeSeconds = songTime - songTimeMinutes * 60;
+      let songTimeSecondsString;
+
+      if (songTimeSeconds < 10) {
+        songTimeSecondsString = `0${songTimeSeconds}`;
+      } else {
+        songTimeSecondsString = songTimeSeconds.toString();
+      }
+
+      console.log(songTime);
+
+      progressBar.style.width = (songTime / songLength) * 100 + "%";
+      currentTimeDisplay.innerHTML = `${songTimeMinutes}:${songTimeSecondsString}`;
+    }
+  }
 }
 
-if (songsList.length == data.length) {
-  const names = document.querySelectorAll(".name");
-  const times = document.querySelectorAll(".time");
-}
+playButton.addEventListener("click", (event) => {
+  if (isFinished) {
+    startProgress();
+  }
+});
