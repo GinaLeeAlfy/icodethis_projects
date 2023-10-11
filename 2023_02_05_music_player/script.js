1bc3dd417f0a749706dde1b74d9a41b54dad0f26
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-10-11 11:41:46 -0700

    fixed the back button and added data

--- 2023_02_05_music_player/script.js
+++ 2023_02_05_music_player/script.js
@@ -1,28 +1,102 @@
 const data = [
   {
-    name: "Someone Lived This",
-    time: 335,
-    artist: "Hans Zimmer",
+    name: "The Greatest Show",
+    time: 303,
+    artist: "Keala Settle, Hugh Jackman, Zac Efron, & Zendaya",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "A Million Dreams",
+    time: 270,
+    artist: "Ziv Zaifmann, Hugh Jackman, & Michelle Ingrid Williams",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "A Million Dreams (Reprise)",
+    time: 61,
+    artist: "Cameron Seely, Austyn Johnson",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "Come Alive",
+    time: 226,
+    artist: "Keala Settle, Daniel Everidge, Hugh Jackman, & Zendaya",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "The Other Side",
+    time: 215,
+    artist: "Hugh Jackman & Zac Efron",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "Never Enough",
+    time: 208,
+    artist: "Loren Allred",
+    src: "",
+    alt: "",
+  },
+  {
+    name: "This Is Me",
+    time: 235,
+    artist: "Keala Settle & The Greatest Showman Ensemble",
+    src: "",
+    alt: "",
+  },
+
+  {
+    name: "Rewrite The Stars",
+    time: 218,
+    artist: "Zac Efron & Zendaya",
+    src: "",
+    alt: "",
+  },
+
+  {
+    name: "Tightrope",
+    time: 235,
+    artist: "Michelle Williams",
+    src: "",
+    alt: "",
+  },
+
+  {
+    name: "Never Enough (Reprise)",
+    time: 81,
+    artist: "Loren Allred",
+    src: "",
+    alt: "",
+  },
+
+  {
+    name: "From Now On",
+    time: 350,
+    artist: "Hugh Jackman & The Greatest Showman Ensemble",
     src: "",
     alt: "",
   },
 ];
 
-// const songContainer = document.querySelector("ul");
-// let songsList = document.querySelectorAll("li");
-// const songTemplate = document.querySelector(".clone");
-// let songClone = songTemplate.cloneNode(true);
+const songContainer = document.querySelector("ul");
+let songsList = document.querySelectorAll("li");
+const songTemplate = document.querySelector(".clone");
+let songClone = songTemplate.cloneNode(true);
 
-// while (songsList.length < data.length) {
-//   songClone = songTemplate.cloneNode(true);
-//   songContainer.appendChild(songClone);
-//   songsList = document.querySelectorAll("li");
-// }
+while (songsList.length < data.length) {
+  songClone = songTemplate.cloneNode(true);
+  songContainer.appendChild(songClone);
+  songsList = document.querySelectorAll("li");
+}
 
-// if (songsList.length == data.length) {
-//   const names = document.querySelectorAll(".name");
-//   const times = document.querySelectorAll(".time");
-// }
+if (songsList.length == data.length) {
+  const names = document.querySelectorAll(".name");
+  const times = document.querySelectorAll(".time");
+}
 
 const progressBar = document.querySelector(".percentage-display");
 const playButton = document.querySelector(".play");
@@ -30,7 +104,7 @@ const currentTimeDisplay = document.querySelector(".current-time");
 const backButton = document.querySelector(".back");
 const slider = document.querySelector(".slider");
 
-let songLength = data[0].time;
+let songLength = data[1].time;
 let songTime = 0;
 
 //set slider starting
@@ -74,11 +148,14 @@ function startProgress() {
   backButton.addEventListener("click", () => {
     clearInterval(time);
     songTime = 0;
+    slider.value = 0;
     isFinished = true;
-    currentTimeDisplay.innerHTML = "0:00";
-    isPaused = false;
-    playButton.innerHTML = `<i class="fa-regular fa-circle-pause fa-2xl"></i>`;
-    startProgress();
+    setDisplays();
+
+    if (isPaused === false) {
+      playButton.innerHTML = `<i class="fa-regular fa-circle-pause fa-2xl"></i>`;
+      startProgress();
+    }
   });
 
   function frame() {
