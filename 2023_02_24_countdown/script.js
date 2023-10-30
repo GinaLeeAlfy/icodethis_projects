6f793a3f87523ea3903d573dc0eb0c647b2ef228
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-10-30 16:02:13 -0700

    if date is changed countdown will reset to new date

--- 2023_02_24_countdown/script.js
+++ 2023_02_24_countdown/script.js
@@ -12,6 +12,9 @@ let monthFormatted;
 let dayFormatted;
 let hourFormatted;
 let minuteFormatted;
+let isNeedClear = false;
+let isStarted = false;
+let countdownTime;
 
 function formatInput() {
   if (month < 10) {
@@ -36,60 +39,88 @@ function formatInput() {
   }
 }
 
+function setDisplays() {
+  let yearsDisplay = Math.floor(countdownTime / 31536000);
+  let monthsDisplay = Math.floor(
+    (countdownTime - yearsDisplay * 31536000) / 2678400
+  );
+  let daysDisplay = Math.floor(
+    (countdownTime - yearsDisplay * 31536000 - monthsDisplay * 2678400) / 86400
+  );
+  let hoursDisplay = Math.floor(
+    (countdownTime -
+      daysDisplay * 86400 -
+      monthsDisplay * 2678400 -
+      yearsDisplay * 31536000) /
+      3600
+  );
+  let minutesDisplay = Math.floor(
+    (countdownTime -
+      hoursDisplay * 3600 -
+      daysDisplay * 86400 -
+      monthsDisplay * 2678400 -
+      yearsDisplay * 31536000) /
+      60
+  );
+  let secondsDisplay =
+    countdownTime -
+    minutesDisplay * 60 -
+    hoursDisplay * 3600 -
+    daysDisplay * 86400 -
+    monthsDisplay * 2678400 -
+    yearsDisplay * 31536000;
+
+  counters[0].innerText = yearsDisplay;
+  counters[1].innerText = monthsDisplay;
+  counters[2].innerText = daysDisplay;
+  counters[3].innerText = hoursDisplay;
+  counters[4].innerText = minutesDisplay;
+  counters[5].innerText = secondsDisplay;
+}
+
 function getDateInput() {
   let dateInputValue = new Date(dateInput.value);
   let dateInputValueTime = dateInputValue.getTime();
+  console.log(dateInputValue);
 
   let currentDate = new Date();
   let currentDateTime = currentDate.getTime();
+  let timeDifferenceMS;
+  let timeDifferenceS;
+  console.log(currentDate);
 
   if (dateInputValueTime > currentDateTime) {
-    let timeDifferenceMS = dateInputValueTime - currentDateTime;
-    let timeDifferenceS = parseInt(timeDifferenceMS / 1000);
+    isStarted = true;
+    timeDifferenceMS = dateInputValueTime - currentDateTime;
+    timeDifferenceS = parseInt(timeDifferenceMS / 1000);
 
     let timer = setInterval(frame, 1000);
-    let countdownTime = timeDifferenceS;
+
+    if (isNeedClear) {
+      clearInterval(timer);
+      isNeedClear = false;
+    }
+    countdownTime = timeDifferenceS;
+
     function frame() {
+      isNeedClear = true;
       if (countdownTime <= 0) {
         clearInterval(timer);
+        isNeedClear = false;
+        isStarted = false;
       } else {
         countdownTime--;
-        console.log(countdownTime);
-        let yearsDisplay = Math.floor(countdownTime / 31536000);
-        let monthsDisplay = Math.floor(
-          (countdownTime - yearsDisplay * 31536000) / 2678400
-        );
-        let daysDisplay = Math.floor(
-          (countdownTime - yearsDisplay * 31536000) / 86400
-        );
-        let hoursDisplay = Math.floor(
-          (countdownTime - daysDisplay * 86400 - yearsDisplay * 31536000) / 3600
-        );
-        let minutesDisplay = Math.floor(
-          (countdownTime -
-            hoursDisplay * 3600 -
-            daysDisplay * 86400 -
-            yearsDisplay * 31536000) /
-            60
-        );
-        let secondsDisplay =
-          countdownTime -
-          minutesDisplay * 60 -
-          hoursDisplay * 3600 -
-          daysDisplay * 86400 -
-          yearsDisplay * 31536000;
-        console.log(secondsDisplay);
-
-        counters[0].innerText = yearsDisplay;
-        counters[1].innerText = monthsDisplay;
-        counters[2].innerText = daysDisplay;
-        counters[3].innerText = hoursDisplay;
-        counters[4].innerText = minutesDisplay;
-        counters[5].innerText = secondsDisplay;
+        setDisplays();
       }
     }
   } else {
     alert("Please choose a date/time in the future.");
+    if (isStarted === true) {
+      isNeedClear = true;
+      countdownTime = 0;
+      setDisplays();
+    }
+    dateInput.value = undefined;
   }
 }
 
