348446b136692d65fd082314e2622296f740ed22
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-11-01 11:51:52 -0700

    added an edit mode with a save data function

--- 2023_02_26_notes_app/script.js
+++ 2023_02_26_notes_app/script.js
@@ -7,12 +7,12 @@ let sessionData = [
   {
     sessionTitle: "Chapter 1",
     sessionNotes:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad neque non ipsam id autem doloribus quae eveniet blanditiis. Aliquam, omnis voluptates. Labore aut iusto fuga exercitationem aperiam facere?\n\nEveniet blanditiis quos itaque, magnam cupiditate ullam! Dolor, fuga sequi eius doloremque, nostrum reiciendis fugiat quasi esse enim similique officiis labore qui magnam? Suscipit obcaecati, assumenda fugiat tenetur vitae aliquam. Ipsum ipsam atque, quas tenetur autem nemo molestias mollitia sapiente harum laboriosam! Quisquam quibusdam tenetur earum, dignissimos ipsa iste, eius possimus distinctio ipsam magni recusandae repellendus cum facere!\n\nAtque at modi quidem culpa consectetur, dolore, vitae dolor fugiat aliquid quae, ut quisquam minus deserunt reiciendis recusandae voluptatum maxime deleniti iste libero corrupti ducimus sit fuga? Quas perspiciatis sunt quia, eligendi, officia exercitationem numquam similique, hic in deserunt natus voluptatum maiores ipsa dolorem? Error, assumenda suscipit? Labore nobis neque amet soluta natus eligendi, consectetur possimus assumenda cumque impedit laudantium culpa. Quidem sit adipisci animi. Assumenda quos omnis tenetur fugiat magnam culpa suscipit! Officiis voluptate ducimus veritatis! Ducimus eveniet expedita corrupti? Dolorem delectus corporis sequi mollitia, molestiae voluptates soluta commodi omnis pariatur id consectetur nemo sit laborum accusantium atque aliquam. Esse, quam excepturi et voluptatem deserunt tempora minima laudantium asperiores sunt quae veniam maxime!\n            ",
+      "Eveniet blanditiis quos itaque, magnam cupiditate ullam! Dolor, fuga sequi eius doloremque, nostrum reiciendis fugiat quasi esse enim similique officiis labore qui magnam? Suscipit obcaecati, assumenda fugiat tenetur vitae aliquam. Ipsum ipsam atque, quas tenetur autem nemo molestias mollitia sapiente harum laboriosam! Quisquam quibusdam tenetur earum, dignissimos ipsa iste, eius possimus distinctio ipsam magni recusandae repellendus cum facere!\n\nAtque at modi quidem culpa consectetur, dolore, vitae dolor fugiat aliquid quae, ut quisquam minus deserunt reiciendis recusandae voluptatum maxime deleniti iste libero corrupti ducimus sit fuga? Quas perspiciatis sunt quia, eligendi, officia exercitationem numquam similique, hic in deserunt natus voluptatum maiores ipsa dolorem? Error, assumenda suscipit? Labore nobis neque amet soluta natus eligendi, consectetur possimus assumenda cumque impedit laudantium culpa. Quidem sit adipisci animi. Assumenda quos omnis tenetur fugiat magnam culpa suscipit! Officiis voluptate ducimus veritatis! Ducimus eveniet expedita corrupti? Dolorem delectus corporis sequi mollitia, molestiae voluptates soluta commodi omnis pariatur id consectetur nemo sit laborum accusantium atque aliquam. Esse, quam excepturi et voluptatem deserunt tempora minima laudantium asperiores sunt quae veniam maxime!\n            ",
   },
   {
     sessionTitle: "Chapter 2",
     sessionNotes:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad neque non ipsam id autem doloribus quae eveniet blanditiis. Aliquam, omnis voluptates. Labore aut iusto fuga exercitationem aperiam facere?\n\nEveniet blanditiis quos itaque, magnam cupiditate ullam! Dolor, fuga sequi eius doloremque, nostrum reiciendis fugiat quasi esse enim similique officiis labore qui magnam? Suscipit obcaecati, assumenda fugiat tenetur vitae aliquam. Ipsum ipsam atque, quas tenetur autem nemo molestias mollitia sapiente harum laboriosam! Quisquam quibusdam tenetur earum, dignissimos ipsa iste, eius possimus distinctio ipsam magni recusandae repellendus cum facere!\n\nAtque at modi quidem culpa consectetur, dolore, vitae dolor fugiat aliquid quae, ut quisquam minus deserunt reiciendis recusandae voluptatum maxime deleniti iste libero corrupti ducimus sit fuga? Quas perspiciatis sunt quia, eligendi, officia exercitationem numquam similique, hic in deserunt natus voluptatum maiores ipsa dolorem? Error, assumenda suscipit? Labore nobis neque amet soluta natus eligendi, consectetur possimus assumenda cumque impedit laudantium culpa. Quidem sit adipisci animi. Assumenda quos omnis tenetur fugiat magnam culpa suscipit! Officiis voluptate ducimus veritatis! Ducimus eveniet expedita corrupti? Dolorem delectus corporis sequi mollitia, molestiae voluptates soluta commodi omnis pariatur id consectetur nemo sit laborum accusantium atque aliquam. Esse, quam excepturi et voluptatem deserunt tempora minima laudantium asperiores sunt quae veniam maxime!\n            ",
+      "Ad neque non ipsam id autem doloribus quae eveniet blanditiis.\n\nEveniet blanditiis quos itaque, magnam cupiditate ullam! Quisquam quibusdam tenetur earum, dignissimos ipsa iste, eius possimus distinctio ipsam magni recusandae repellendus cum facere!\n\nAtque at modi quidem culpa consectetur, dolore, vitae dolor fugiat aliquid quae, ut quisquam minus deserunt reiciendis recusandae voluptatum maxime deleniti iste libero corrupti ducimus sit fuga? Quas perspiciatis sunt quia, eligendi, officia exercitationem numquam similique, hic in deserunt natus voluptatum maiores ipsa dolorem? Error, assumenda suscipit? Labore nobis neque amet soluta natus eligendi, consectetur possimus assumenda cumque impedit laudantium culpa. Quidem sit adipisci animi. Assumenda quos omnis tenetur fugiat magnam culpa suscipit! Officiis voluptate ducimus veritatis! Ducimus eveniet expedita corrupti? Dolorem delectus corporis sequi mollitia, molestiae voluptates soluta commodi omnis pariatur id consectetur nemo sit laborum accusantium atque aliquam. Esse, quam excepturi et voluptatem deserunt tempora minima laudantium asperiores sunt quae veniam maxime!\n            ",
   },
 ];
 
@@ -27,6 +27,9 @@ const sessionContainer = document.querySelector("ul");
 let sessions = document.querySelectorAll("li");
 const sessionTemplate = document.querySelector(".clone");
 let sessionClone = sessionTemplate.cloneNode(true);
+
+let currentIndex = 0;
+let isEditing = false;
 
 function updateSessions() {
   while (sessions.length < sessionData.length) {
@@ -43,15 +46,47 @@ function updateSessions() {
       const element = sessionData[index];
 
       sessionTitles[index].innerHTML = element.sessionTitle;
+      sessions[index].dataset.indexNumber = index;
     }
   }
 }
 
 sessionContainer.addEventListener("click", (event) => {
-  console.log(event.target.tagName);
-
+  saveData();
   if (event.target.tagName === "LI") {
+    let index = event.target.dataset.indexNumber;
+    sessions.forEach((element) => {
+      element.classList.remove("selected");
+    });
+    sessions[index].classList.add("selected");
+    sessionTitleEl.value = sessionData[index].sessionTitle;
+    sessionNotesEl.value = sessionData[index].sessionNotes;
+    currentIndex = index;
   }
 });
 
+editButton.addEventListener("click", () => {
+  if (isEditing === false) {
+    isEditing = true;
+    titleEl.removeAttribute("readonly");
+    sessionTitleEl.removeAttribute("readonly");
+    sessionNotesEl.removeAttribute("readonly");
+  } else {
+    isEditing = false;
+    titleEl.setAttribute("readonly", true);
+    sessionTitleEl.setAttribute("readonly", true);
+    sessionNotesEl.setAttribute("readonly", true);
+    saveData();
+  }
+});
+
+function saveData() {
+  sessionData[currentIndex].sessionTitle = sessionTitleEl.value;
+  sessionData[currentIndex].sessionNotes = sessionNotesEl.value;
+}
+
+//load page
 updateSessions();
+sessions[0].classList.add("selected");
+sessionTitleEl.value = sessionData[0].sessionTitle;
+sessionNotesEl.value = sessionData[0].sessionNotes;
