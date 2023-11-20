cae54bef6fe13d87fd5b35028576f04908ad6af5
Gina Alfandary Magana <ginaleealfy@gmail.com>
2023-11-20 10:59:45 -0800

    added script to open and collapse questions. added media query for desktop

---
+++ 2023_03_15_accordion_2/script.js
@@ -0,0 +1,26 @@
+const section = document.querySelector("section");
+const articles = document.querySelectorAll("article");
+const expandButton = document.querySelectorAll(".question button");
+
+section.addEventListener("click", (event) => {
+  let target = event.target;
+  if (target.tagName === "ARTICLE") {
+    if (!target.classList.contains("open")) {
+      articles.forEach((article, index) => {
+        article.classList.remove("open");
+        expandButton[index].innerHTML = "&#43;";
+      });
+      target.classList.add("open");
+      expandButton[target.dataset.indexNumber].innerHTML = "&#8722;";
+    } else {
+      articles.forEach((article, index) => {
+        article.classList.remove("open");
+        expandButton[index].innerHTML = "&#43;";
+      });
+    }
+  }
+
+  if (target.tagName === "BUTTON") {
+    alert("Will direct to another page");
+  }
+});
